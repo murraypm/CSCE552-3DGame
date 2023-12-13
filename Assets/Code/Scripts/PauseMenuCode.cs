d64e7bb720fbe5aad4aea8ce07a972d18978bfcf
Logan <95957046+Clnall@users.noreply.github.com>
2023-12-12 20:10:40 -0500

    pauseMenu

--- Assets/Code/Scripts/PauseMenuCode.cs
+++ Assets/Code/Scripts/PauseMenuCode.cs
@@ -7,6 +7,9 @@ public class PauseMenuCode : MonoBehaviour
 {
     public GameObject PauseMenu;
     public static bool isPaused = false;
+    [SerializeField] private GameObject Options;
+    [SerializeField] private GameObject PauseStuff;
+    [SerializeField] private GameObject HowToPlay;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +25,14 @@ public class PauseMenuCode : MonoBehaviour
         {
             if (isPaused)
             {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
                 ResumeGame();
             }
             else
             {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
                 PauseGame();
             }
         }
@@ -40,6 +47,13 @@ public class PauseMenuCode : MonoBehaviour
 
     public void ResumeGame()
     {
+        Options.SetActive(false);
+        HowToPlay.SetActive(false);
+        PauseStuff.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+
         PauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
@@ -47,6 +61,6 @@ public class PauseMenuCode : MonoBehaviour
 
     public void QuitGame(){
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("Menus");
     }
 }
