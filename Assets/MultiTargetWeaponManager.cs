a471389fa8df42c8975358b50fd4399e03762548
murraypm <murraypm@email.sc.edu>
2023-12-12 22:45:26 -0500

    Multi target weapon script

---
+++ Assets/MultiTargetWeaponManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTargetWeaponManager : MonoBehaviour
+{
+    public float attackCoolDown = 2.0f;
+    public int attackDamage = 2;
+    float currentCoolDown = 0.0f;
+    bool onCoolDown = false;
+    List<GameObject> enemiesInReach = new List<GameObject>();
+    // public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lineRenderer = this.GetComponentInChildren<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: Clean up this logic it's a mess and can definitely be optimized
+        if (enemiesInReach.Count > 0)
+        {
+            //lineRenderer.SetPosition(0, this.transform.position);
+            //lineRenderer.SetPosition(1, enemiesInReach[0].transform.position);
+        }
+        if (!onCoolDown && enemiesInReach.Count > 0)
+        {
+            // TODO: change so it loops through all enemies in reach
+            // and subtracts health instead of just first one
+            onCoolDown = true;
+            currentCoolDown = 0.0f;
+            GameObject enemy = enemiesInReach[0];
+            EnemyManager enemyManager = enemy.GetComponent<EnemyManager>();
+            enemyManager.health -= attackDamage;
+            //print("Enemy hit");
+            if (enemyManager.health < 0.0)
+            {
+                enemiesInReach.Remove(enemy);
+                Destroy(enemy);
+            }
+        }
+        else if (currentCoolDown < 2.0f)
+        {
+            currentCoolDown += Time.deltaTime;
+        }
+        else if (currentCoolDown >= attackCoolDown)
+        {
+            onCoolDown = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!enemiesInReach.Contains(other.gameObject))
+        {
+            enemiesInReach.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (enemiesInReach.Contains(other.gameObject))
+        {
+            enemiesInReach.Remove(other.gameObject);
+        }
+    }
+}
